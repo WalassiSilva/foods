d59aef4bdee46d4a497cf27b9932cd0bc03ff75b
Walassi Silva <walassi.b@gmail.com>
2024-05-30 17:17:05 -0300

    feat: add initial header component

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Header from "./_components/Header";
+
 export default function Home() {
   return (
-    <main className="mb-5 bg-red-500 px-4">
-      <h1>hello tailwind</h1>
+    <main className="">
+      <Header />
     </main>
   );
 }
