126e025e45a635a8f17cbc2a5b1f435bfa8ed1c6
Walassi Silva <walassi.b@gmail.com>
2024-06-01 17:17:11 -0300

    feat: add link to home at logo image

--- app/_components/Header.tsx
+++ app/_components/Header.tsx
@@ -1,11 +1,22 @@
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { MenuIcon } from "lucide-react";
+import Link from "next/link";
 
 export default function Header() {
   return (
     <div className="flex justify-between px-5 pt-6">
-      <Image src="/logo.png" height={30} width={100} alt="FSW Foods" />
+      <div className="relative h-[30px] w-[100px]">
+        <Link href="/">
+          <Image
+            src="/logo.png"
+            height={30}
+            width={100}
+            alt="FSW Foods"
+            className="object-cover"
+          />
+        </Link>
+      </div>
       <Button
         size="icon"
         variant="outline"
