c94e1b26497ae4026409c60fe6caac54d20e90f9
Walassi Silva <walassi.b@gmail.com>
2024-06-01 19:36:57 -0300

    feat: add restaurants search

--- app/_components/search.tsx
+++ app/_components/search.tsx
@@ -1,15 +1,38 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 import { Input } from "./ui/input";
 import { Button } from "./ui/button";
 import { SearchIcon } from "lucide-react";
+import { useRouter } from "next/navigation";
 
 export default function Search() {
+  const [search, setSearch] = useState("");
+  const router = useRouter();
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+    console.log(search);
+  };
+
+  const handleSearchSubmit: React.FormEventHandler<HTMLFormElement> = (e) => {
+    e.preventDefault();
+    if (!search) return;
+
+    router.push(`/restaurants?search=${search}`);
+  };
+
   return (
-    <div className="flex gap-2">
-      <Input placeholder="Buscar restaurantes" className="border-none" />
-      <Button size="icon">
+    <form onSubmit={handleSearchSubmit} className="flex gap-2">
+      <Input
+        placeholder="Buscar restaurantes"
+        className="border-none"
+        onChange={handleChange}
+        value={search}
+      />
+      <Button type="submit" size="icon">
         <SearchIcon size={20} />
       </Button>
-    </div>
+    </form>
   );
 }
